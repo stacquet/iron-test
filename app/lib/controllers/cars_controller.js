8067fadba1db8c7320a4fbbac8bb387e30a349c0
stacquet <sylvain.tacquet@gmail.com>
2015-07-20 22:33:08 +0200

    first commit

---
+++ app/lib/controllers/cars_controller.js
@@ -0,0 +1,40 @@
+CarsController = RouteController.extend({
+  subscriptions: function () {
+    this.subscribe('cars');
+    // set up the subscriptions for the route and optionally
+    // wait on them like this:
+    //
+    // this.subscribe('item', this.params._id).wait();
+    //
+    // "Waiting" on a subscription does not block. Instead,
+    // the subscription handle is added to a reactive list
+    // and when all items in this list are ready, this.ready()
+    // returns true in any of your route functions.
+  },
+
+  data: function () {
+    // return a global data context like this:
+    // Items.findOne({_id: this.params._id});
+    return Cars.findOne({_id: this.params._id});
+  },
+  create: function() {
+    this.render('CreateCar', {});
+  },
+
+  action: function () {
+    // You can create as many action functions as you'd like.
+    // This is the primary function for running your route.
+    // Usually it just renders a template to a page. But it
+    // might also perform some conditional logic. Override
+    // the data context by providing it as an option in the
+    // last parameter.
+    this.render('Cars', { /* data: {} */});
+  },
+  list: function() {
+    this.render('ListCars', {});
+  },
+
+  edit: function() {
+  this.render('EditCar', {});
+  }
+});
